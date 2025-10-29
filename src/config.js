ebc026868557b11572409c3994b668f4e935e28b
Ujval <ujval99@gmail.com>
2025-10-30 02:08:18 +0530

    all files

---
+++ src/config.js
@@ -0,0 +1,2 @@
+export const BRAND = { name: 'Ocean Magics', logo: '/logo.svg' }
+export const IMAGES = { hero: 'https://images.unsplash.com/photo-1507525428034-b723cf961d3e?auto=format&fit=crop&w=2000&q=60' }
